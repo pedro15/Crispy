329361f4d236593d56ec3ee9a2366f48321237b7
Pedro Duran <pedrojosuedm@hotmail.com>
2024-03-03 16:25:34 -0400

    message display improved

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ int main(int argc, char* argv[])
 {
     std::cout << ENGINE_HEADER  << std::endl;
     std::cout << "VERSION: " << ENGINE_VERSION << std::endl;
-    std::cout << "Type 'help' to display available commands" << std::endl;
+    std::cout << "Type 'help' to display available commands" << std::endl << std::endl;
 
     uci uci_loop = uci();
     uci_loop.Run();
-    
+
     helpcommand help_command = helpcommand(uci_loop);
     help_command.Execute(std::vector<std::string>());
     
