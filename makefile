b074982dbbfbd9544a8da4ac98e8a7a139ed4533
Pedro Duran <pedrojosuedm@hotmail.com>
2024-03-30 23:21:29 -0400

    update makefile and ANCII ART

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ OUTPUT = Crispy.exe
 FLAGS_GLOBAL = -std=c++20
 FLAGS_RELEASE = -O3
 FLAGS_DEBUG = -g
-SOURCES = src/Main.cpp src/UciClient.cpp src/commands/CommandBase.cpp src/commands/HelpCommand.cpp
+SOURCES = src/*.cpp src/commands/*.cpp
 
 build:
 	g++ $(SOURCES) $(FLAGS_GLOBAL) $(FLAGS_RELEASE) -o $(OUTPUT)
--- src/Types.h
+++ src/Types.h
@@ -4,6 +4,6 @@ const char* ENGINE_ANCII_ART =
 "  __    ___    _   ___   ___  __ __    \n" 
 " / _|  | o \\  | | / __) | o | \\ V /  \n" 
 "( (_   |   /  | | \\__ \\ |  _/  \\ /  \n"  
-" \\__|  |_| \\  |_| (___/ |_|    |_|   \n";
+" \\__|  |_|_\\  |_| (___/ |_|    |_|   \n";
 
-const char* ENGINE_VERSION = "0.5";
+const char* ENGINE_VERSION = "0.2";
