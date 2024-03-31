8a72ebb0a06ae8035dd7ede8e96922bf9e3ae143
Pedro Duran <pedrojosuedm@hotmail.com>
2024-03-31 12:18:46 -0400

    update HelpCommand

--- src/commands/HelpCommand.cpp
+++ src/commands/HelpCommand.cpp
@@ -4,7 +4,7 @@ HelpCommand::HelpCommand(UciClient* uci_client) : CommandBase(uci_client) { }
 
 void HelpCommand::Execute(const std::vector<std::string> params)
 {
-    std::cout << " Crispy is meant to be used with a graphical user interface (GUI) and implements the Universal Chess Interface (UCI) protocol to communicate with a GUI, etc.." << std::endl;
+    std::cout << " Crispy is meant to be used with a graphical user interface (GUI) \n and implements the Universal Chess Interface (UCI) protocol to communicate with a GUI, etc.." << std::endl;
     std::cout << " please refer to HomePage (" << HOMEPAGE << ") for more details about available commands." << std::endl;
 }
 
