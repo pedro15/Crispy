dce6d0a5f5f11f69681fb81a7d88041a4165bfc2
Pedro Duran <pedrojosuedm@hotmail.com>
2024-04-14 17:42:17 -0400

    make GetFENFromCMD constant function

--- src/Notation.h
+++ src/Notation.h
@@ -6,8 +6,8 @@ namespace notation
 {
     const std::string FEN_STARTPOS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     const std::string FEN_KIWIPETE = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
-
-    static std::string GetFENFromCMD(const std::vector<std::string> cmd_args)
+    
+    static std::string GetFENFromCMD(const std::vector<std::string> cmd_args) const
     {
         std::stringstream result;
         for (int i = 0; i < cmd_args.size(); i++)
