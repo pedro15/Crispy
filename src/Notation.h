58607c6006c8d5e9f9f30ff02a988cd05cd26053
Pedro Duran <pedrojosuedm@hotmail.com>
2024-04-14 17:42:57 -0400

    make GetFENFromCMD function inlined

--- src/Notation.h
+++ src/Notation.h
@@ -7,7 +7,7 @@ namespace notation
     const std::string FEN_STARTPOS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     const std::string FEN_KIWIPETE = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
     
-    static std::string GetFENFromCMD(const std::vector<std::string> cmd_args) const
+    inline static std::string GetFENFromCMD(const std::vector<std::string> cmd_args)
     {
         std::stringstream result;
         for (int i = 0; i < cmd_args.size(); i++)
