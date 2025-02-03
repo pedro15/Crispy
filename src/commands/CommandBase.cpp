82a6ba42b2f5c79bf16a6e7d0c3734b127d867a6
Pedro Duran <pedrojosuedm@hotmail.com>
2025-02-02 21:25:12 -0400

    refactored command pattern

--- src/commands/CommandBase.cpp
+++
@@ -1,8 +0,0 @@
-#include "CommandBase.h"
-#include <iostream>
-#include <vector>
-#include <sstream>
-
-CommandBase::CommandBase(std::shared_ptr<UciClient> _uci_client) : m_uci_client(_uci_client){ }
-CommandBase::~CommandBase() { }
-void CommandBase::Execute(const std::vector<std::string> _params){ }
